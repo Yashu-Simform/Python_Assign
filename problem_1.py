93197c0895ee1999733f13754966168d742d56a3
Yashu Ranparia <yashu.ranparia@simformsolutions.com>
2025-02-19 18:49:14 +0530

    Init

---
+++ problem_1.py
@@ -0,0 +1,9 @@
+class CalcGCD:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        pass
+
+    def processData(self, data):
+        
+        pass
---
+++ problem_3.py
@@ -0,0 +1,55 @@
+# from collections import defaultdict, Counter
+
+def importer(func):
+    def wrapper(self,lst):
+        from collections import defaultdict, Counter
+        print('Imported sucessfully!')
+        func(self, lst)
+
+    return wrapper
+
+#Approach1: compare all string and group similar ones after sorting
+#Approach2: Make counter and compare counters if same append to list otherwise create a new list
+#Approach3: 
+
+
+class Accumulator:
+
+    @importer
+    def __init__(self, lst):
+        self.lst = lst
+        self.result = defaultdict(list)
+        pass
+
+    def approach1(self):
+        for s in self.lst:
+            ts = ''.join(sorted(s))
+            self.result[ts].append(s)
+        pass
+
+    def splitStr(self, s):
+        return [i for i in s]
+    
+    @importer
+    def formSet(self):
+        for s in self.lst:
+            d = Counter(self.splitStr(s)).items()
+            l = sorted(list(d))
+            self.result[tuple(l)].append(s)
+
+    def getResult(self):
+        # self.formSet()
+        self.approach1()    #O(n * klogk)
+        return list(self.result.values())
+    
+inp = ["eat","tea","tan","ate","nat","bat"]
+testCases = [
+    ["eat","tea","tan","ate","nat","bat"],
+    [''],
+    ['aa', 'aaa'],
+]
+
+ac = Accumulator(inp)
+result = ac.getResult()
+
+print(result)
